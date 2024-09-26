1658014e6e78e605096fe0d0a0b630301cb10a70
Haidon Cosse <Haidonmcosse@gmail.com>
2024-09-26 10:38:27 -0500

    rope

--- Assets/Scripts/RopeBuilder.cs
+++ Assets/Scripts/RopeBuilder.cs
@@ -1,6 +1,4 @@
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class RopeBuilder : MonoBehaviour
@@ -13,13 +11,15 @@ public class RopeBuilder : MonoBehaviour
     private float m_Length;
     [SerializeField]
     private int m_segmentNumber;
+
     // Start called when the script instance is being loaded
     private void Start()
     {
         // Initialize rope segments list
         m_segments = new List<GameObject>();
+
         // Set segment length and number of segments
-        if(m_Length == 0)
+        if (m_Length == 0)
         {
             m_Length = 1;
         }
@@ -27,34 +27,53 @@ public class RopeBuilder : MonoBehaviour
         {
             m_segmentNumber = 1;
         }
+
         // Build the rope
         BuildRope();
     }
+
     // Build the rope
     private void BuildRope()
     {
-        // Loop through the number of segments
         for (int i = 0; i < m_segmentNumber; i++)
         {
             // Create a new segment
             GameObject m_segment = Instantiate(m_ropePrefab);
-            
+
             // Calculate the position of the segment
             Vector3 m_position = CalculateSegmentPosition(i);
-            // Set segment's position
             m_segment.transform.position = m_position;
-            // Add segment to the list
-            m_segments.Add(m_segment);
-        }
 
+            // Make the first segment kinematic
+            if (i == 0)
+            {
+                m_segment.GetComponent<Rigidbody>().isKinematic = true;
+            }
+
+            // Add the segment to the list
+            m_segments.Add(m_segment);
+
+            // Connect to the previous segment if it's not the first one
+            if (i > 0)
+            {
+                ConnectSegments(m_segments[i - 1], m_segment);
+            }
+        }
     }
 
-    //Calculate the position of each rope segment
+    // Calculate the position of each rope segment
     private Vector3 CalculateSegmentPosition(int index)
     {
-        //Calculate the position based on the index and segment length
         return new Vector3(0, index * m_Length, 0);
     }
+
+    // Connect two segments with a joint
+    private void ConnectSegments(GameObject previousSegment, GameObject currentSegment)
+    {
+        FixedJoint joint = currentSegment.AddComponent<FixedJoint>();
+        joint.connectedBody = previousSegment.GetComponent<Rigidbody>();
+    }
+
     // Optional: Update rope dynamically (e.g., on user input)
     private void Update()
     {
@@ -67,40 +86,49 @@ public class RopeBuilder : MonoBehaviour
             RemoveSegment();
         }
     }
-    // Check for input to modify the rope (add/remove segments)
 
-    //Add a segment to the rope
+    // Add a segment to the rope
     private void AddSegment()
     {
-        for (int i = 0; i < m_segmentNumber;)
+        int index = m_segments.Count;
+
+        // Create a new segment
+        GameObject m_segment = Instantiate(m_ropePrefab);
+
+        // Calculate the position of the segment
+        Vector3 m_position = CalculateSegmentPosition(index);
+        m_segment.transform.position = m_position;
+
+        // Make the new segment dynamic
+        m_segment.GetComponent<Rigidbody>().isKinematic = false;
+
+        // Add the new segment to the list
+        m_segments.Add(m_segment);
+
+        // Connect to the previous segment
+        if (index > 0)
         {
-            // Create a new segment
-            GameObject m_segment = Instantiate(m_ropePrefab);
-
-            // Calculate the position of the segment
-            Vector3 m_position = CalculateSegmentPosition(i);
-            // Set segment's position
-            m_segment.transform.position = m_position;
-            // Add segment to the list
-            m_segments.Add(m_segment);
+            ConnectSegments(m_segments[index - 1], m_segment);
         }
-
     }
 
-    //Remove the last segment of the rope
+    // Remove the last segment of the rope
     private void RemoveSegment()
     {
         // Check if there are segments to remove
-        if (m_segments.Count <= 0)
+        if (m_segments.Count > 0)
         {
             // Destroy the last segment
-            GameObject m_lastSegment = m_segments.Last();
+            GameObject m_lastSegment = m_segments[m_segments.Count - 1];
             Destroy(m_lastSegment);
-            // Remove it from the list
             m_segments.RemoveAt(m_segments.Count - 1);
+
+            // Reconnect the last remaining segment to the one before it if necessary
+            if (m_segments.Count > 0)
+            {
+                // You might want to add logic to reconnect the remaining segments here
+                // This could involve removing the joint from the last segment and reconnecting it to the new last segment
+            }
         }
-
     }
-
-
 }
